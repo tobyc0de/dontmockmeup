9708771745ac4c48abe013d5ef69e71a09be612b
Tobias Foit <tobias.foit@gmail.com>
2023-09-21 13:32:35 +0100

    simplified poker

--- strippoker.js
+++ strippoker.js
@@ -1,11 +1,16 @@
 // Strip poker
 
 let clothesItems = ["shoes", "trousers", "shirt", "hat", "jacket"];
-let userPoints = 0;
-let numberOfItemsRemoved = 0;
 let selectedItem = "nothing";
 let indexOfSelected;
 let numberOfItemsLeft = clothesItems.length;
+let itemList = returnItem();
+
+function returnItem() {
+  for (let x = 0; x < clothesItems.length; x++) {
+    console.log("clothesItemsx: " + clothesItems[x]);
+  }
+}
 
 function addClothing() {
   document.querySelector("#currentoutfit").innerHTML = clothesItems;
@@ -17,18 +22,13 @@ function addClothing() {
       ". What do you want to add to your outfit? "
   );
   clothesItems.push(addedItem);
-  console.log(clothesItems);
   numberOfItemsLeft = clothesItems.length;
   document.querySelector("#currentoutfit").innerHTML =
     "You are currently wearing: " + clothesItems;
 }
 
 function removeClothing() {
-  console.log("number of items: " + numberOfItemsRemoved);
-  console.log("numer of items left: " + numberOfItemsLeft);
-  console.log("clothesItems.length: " + clothesItems.length);
-
-  while (numberOfItemsLeft >= clothesItems.length && numberOfItemsLeft !== 0) {
+  if (numberOfItemsLeft >= clothesItems.length && numberOfItemsLeft !== 0) {
     selectedItem = prompt(
       "Please remove one of the following items: " + clothesItems
     ).toLowerCase();
@@ -36,25 +36,14 @@ function removeClothing() {
     for (let i = 0; i < 5; i++) {
       if (selectedItem === clothesItems[i]) {
         alert("you removed your " + selectedItem + "! Well done.");
-        userPoints++;
-        numberOfItemsRemoved++;
-        numberOfItemsLeft--;
         indexOfSelected = clothesItems.indexOf(selectedItem);
         clothesItems.splice(indexOfSelected, 1);
+        numberOfItemsLeft = clothesItems.length;
         document.querySelector("#currentoutfit").innerHTML =
           "You are currently wearing: " + clothesItems;
-        console.log("number of items removed: " + numberOfItemsRemoved);
-        console.log("numer of items left: " + numberOfItemsLeft);
-        console.log("clothesItems.length: " + clothesItems.length);
       }
     }
+  } else {
+    alert("You are already naked! Better put some more clothes on.");
   }
-
-  alert(
-    "You are now naked, congratulations! You removed " +
-      userPoints +
-      " out of " +
-      numberOfItemsRemoved +
-      " clothing items."
-  );
 }
